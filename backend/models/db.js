1d04c6709d7fd222af433b6f84af50cb57198cd1
DiegoOrtegaElRincon <diegoalejandroortegamanzano@alumno.ieselrincon.es>
2023-11-28 17:06:59 +0000

    CRUD for Administrators is done

--- backend/models/db.js
+++ backend/models/db.js
@@ -15,22 +15,17 @@ const sequelize = new Sequelize(database, user, password, {
     },
 });
 
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Database connection has been established successfully.');
-    })
-    .catch((err) => {
-        console.error('Unable to connect to the database:', err);
-    });
+sequelize.authenticate().then(() => {
+    console.log('Database connection has been established successfully.');
+}).catch((err) => {
+    console.error('Unable to connect to the database:', err);
+});
 
-sequelize.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`)
-    .then(() => {
-        console.log('Database created or already exists');
-    })
-    .catch((err) => {
-        console.error('Error creating database:', err);
-    });
+sequelize.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`).then(() => {
+    console.log('Database created or already exists');
+}).catch((err) => {
+    console.error('Error creating database:', err);
+});
 
 const db = {};
 db.Sequelize = Sequelize;
@@ -41,6 +36,7 @@ db.Spot = require('./Spot.js')(sequelize, Sequelize);
 db.ActiveElement = require('./ActiveElement.js')(sequelize, Sequelize);
 db.ChangingElement = require('./ChangingElement.js')(sequelize, Sequelize);
 db.PassiveElement = require('./PassiveElement.js')(sequelize, Sequelize);
+db.Admin = require('./Admin.js')(sequelize, Sequelize);
 
 // Sincronizar todos los modelos con la base de datos
 sequelize.sync({ force: true }).then(() => {
