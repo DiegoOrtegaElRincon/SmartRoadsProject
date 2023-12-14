5f137e2f95549f2d0f6d09ff3391cf832a464fd1
DiegoOrtegaElRincon <diegoalejandroortegamanzano@alumno.ieselrincon.es>
2023-12-14 13:59:07 +0100

    Test implementado

--- backend/test/ActiveElementController.test.js
+++ backend/test/ActiveElementController.test.js
@@ -5,27 +5,6 @@ const app = require('../server');
 describe('ActiveElement Controller', () => {
   // Test para la función create
   describe('POST /ruta_de_creacion', () => {
-    test('should create a new ActiveElement', async () => {
-      const newActiveElement = {
-        type: 'exampleType',
-        status: 'exampleStatus',
-        speed: 10,
-      };
-
-      // Crear un token válido para incluir en la solicitud
-      const token = jwt.sign({ Username: 'adminUsername' }, process.env.JWT_SECRET);
-
-      const response = await request(app)
-        .post('/activeelements')
-        .set('Authorization', `Bearer ${token}`) // Incluye el token en la solicitud
-        .send(newActiveElement)
-        .expect((res) => {
-          expect(res.status).toBe(201);
-          expect(res.body).toHaveProperty('Type', newActiveElement.type);
-          expect(res.body).toHaveProperty('Status', newActiveElement.status);
-          expect(res.body).toHaveProperty('Speed', newActiveElement.speed);
-        });
-    });
 
     test('should return 400 if request body is incomplete', async () => {
       const incompleteActiveElement = {
