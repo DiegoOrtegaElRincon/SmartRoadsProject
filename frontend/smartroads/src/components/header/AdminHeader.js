f1e1313b718696716df8c144be7c09fc90654f5e
DiegoOrtegaElRincon <diegoalejandroortegamanzano@alumno.ieselrincon.es>
2023-12-14 23:23:37 +0000

    worked in class

--- frontend/smartroads/src/components/header/AdminHeader.js
+++ frontend/smartroads/src/components/header/AdminHeader.js
@@ -1,36 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './AdminHeader.css'; // Make sure to link the new CSS file
 
 const AdminHeader = () => {
     return (
-        <div>
-            <nav className="navbar navbar-expand navbar-dark bg-dark">
-                <a href="/admins" className="navbar-brand">
-                    Admin View
-                </a>
-                <div className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <Link to={"/admins"} className="nav-link">
-                            Admins
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={"/activeelements"} className="nav-link">
-                            Active Element
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={"/changingelements"} className="nav-link">
-                            Changing Element
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={"/passiveelements"} className="nav-link">
-                            Passive Element
-                        </Link>
-                    </li>
+        <div className="custom-nav-container">
+            <div className="custom-nav">
+                <div className="nav-items">
+                    <Link to={"/admins"} className="nav-item-link">
+                        Admins
+                    </Link>
+                    <Link to={"/activeelements"} className="nav-item-link">
+                        ActiveE
+                    </Link>
+                    <Link to={"/changingelements"} className="nav-item-link">
+                        ChangingE
+                    </Link>
+                    <Link to={"/passiveelements"} className="nav-item-link">
+                        PassiveE
+                    </Link>
                 </div>
-            </nav>
+            </div>
         </div>
     );
 };
--- frontend/smartroads/src/components/login/Login.js
+++ frontend/smartroads/src/components/login/Login.js
@@ -8,13 +8,23 @@ const LoginForm = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
+  const validateInput = (input, type) => {
+    const regex = /^[A-Za-z0-9_$#&]{0,12}$/; // Allow empty and up to 12 valid characters
+    if (regex.test(input)) {
+      if (type === 'username') {
+        setUsername(input);
+      } else if (type === 'password') {
+        setPassword(input);
+      }
+    }
+  };
+
   const handleLogin = async () => {
     try {
-      const response = await AuthService.signIn(username, password);
-
-      localStorage.setItem('userInfo', JSON.stringify(response));
-
-      window.location.href = '/activeelements';
+      const response = await AuthService.signIn(username, password).then(next => {
+        localStorage.setItem('userInfo', JSON.stringify(response))
+        window.location.href = '/activeelements'
+      });
     } catch (error) {
       console.error('Error de inicio de sesión:', error);
       setError('Usuario o contraseña incorrectos');
@@ -31,9 +41,9 @@ const LoginForm = () => {
             type="text"
             className="form-control"
             id="username"
-            placeholder="Introduce email"
+            placeholder="Introduce username"
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={(e) => validateInput(e.target.value, 'username')}
           />
         </div>
         <div className="form-group">
@@ -43,13 +53,10 @@ const LoginForm = () => {
             id="password"
             placeholder="Introduce password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => validateInput(e.target.value, 'password')}
           />
         </div>
-        <button
-          className="button button1"
-          onClick={handleLogin}
-        >Login</button>
+        <button className="button" onClick={handleLogin}>Login</button>
       </form>
       <Footer />
     </div>
